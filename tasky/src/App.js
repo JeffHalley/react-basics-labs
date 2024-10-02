66f1dfe6594b16d502a3b74cbc7b3d560fbba6be
JeffHalley <116006784+JeffHalley@users.noreply.github.com>
2024-10-02 13:24:32 +0100

    tasky lab2 - excersie done

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -8,9 +8,9 @@ import { v4 as uuidv4 } from 'uuid';
 function App() {
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false },
-      { id: 3, title: "Tidy up", deadline: "Today", done: false}
+      { id: 1, title:"Dishes", description: "Empty dishwasher",priority:"low", deadline: "Today", done: false },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away",priority:"medium", deadline: "Tomorrow", done: false },
+      { id: 3, title: "Tidy up",priority:"high", deadline: "Today", done: false}
     ]
   });
   const doneHandler = (taskIndex) => {
@@ -28,7 +28,8 @@ function App() {
   const [ formState, setFormState ] = useState({
     title: "",
     description: "",
-    deadline: ""
+    deadline: "",
+    priority:""
   });
 
 
@@ -45,6 +46,9 @@ function App() {
           break;
       case "deadline":
           form.deadline = event.target.value;
+          break;
+      case "priority":
+          form.priority = event.target.value;
           break;
       default:
           form = formState;
@@ -72,6 +76,7 @@ function App() {
     <Task 
       title={task.title}
       description={task.description}
+      priority={task.priority}
       deadline={task.deadline}
       key={task.id}
       done={task.done}
